6271c782738cf27e8349612d56a6d70e9b13704d
vrk555442 <vudayanaravikumar@gmail.com>
2023-11-05 21:11:43 +0530

    Bug Fix and included App Icon

--- app/src/main/java/com/rk/newsapp/data/network/ApiService.kt
+++ app/src/main/java/com/rk/newsapp/data/network/ApiService.kt
@@ -7,7 +7,7 @@ import retrofit2.http.Query
 
 interface ApiService {
 
-    @GET("/top-headlines")
+    @GET("top-headlines")
     suspend fun getNewsArticles(@Query("country") country: String = "us",
     @Query("apikey") apikey: String = "0c8ae7d565dc45589367b37bf675e87d") : Response<NewsDTO>
 }
